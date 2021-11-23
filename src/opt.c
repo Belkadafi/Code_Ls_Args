eda2700c5f0f4cb1ce79e8786603ad1bf48d47be
Belkadafi <merzouk.rafik@epitech.eu>
2021-11-23 22:59:59 +0100

    initial commit

---
+++ src/opt.c
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2021
+** Code_Ls_Args
+** File description:
+** opt
+*/
+
+#include "my.h"
+
+void ls_option_stat(char **av __attribute__((unused)), option_t *opt __attribute__((unused)))
+{
+}
